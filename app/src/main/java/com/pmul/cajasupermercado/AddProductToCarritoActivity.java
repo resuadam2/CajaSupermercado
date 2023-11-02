53d9af28a846a4c1b7b18d595abaf787296c1993
resuadam2 <resuadam2@gmail.com>
2023-11-02 18:44:42 +0100

    solving some bugs and starting logic for add a product

--- app/src/main/java/com/pmul/cajasupermercado/AddProductToCarritoActivity.java
+++ app/src/main/java/com/pmul/cajasupermercado/AddProductToCarritoActivity.java
@@ -1,9 +1,14 @@
 package com.pmul.cajasupermercado;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,6 +25,9 @@ public class AddProductToCarritoActivity extends AppCompatActivity {
 
     private DBManager dbManager;
 
+    private EditText etCant;
+
+    private Button btAddToCarrito;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -40,17 +48,58 @@ public class AddProductToCarritoActivity extends AppCompatActivity {
         );
 
         lvProductos.setAdapter(this.adaptadorProductos);
+
+        lvProductos.setOnItemClickListener((parent, view, position, id) -> selectProduct(position));
+
+        btAddToCarrito = (Button) this.findViewById(R.id.btAdd);
+        btAddToCarrito.setEnabled(false); //Hasta que haya un elemento seleccionado no podemos añadir nada
+
+        btAddToCarrito.setOnClickListener(v -> addProductToCarrito());
+
+        etCant = (EditText) this.findViewById(R.id.etCant);
+
+        etCant.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s.length() == 0 || Integer.parseInt(s.toString()) == 0) btAddToCarrito.setEnabled(false);
+                else btAddToCarrito.setEnabled(true);
+            }
+        });
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        // Recorremos el adaptador para ver el stock de cada producto
-
-
+    private void selectProduct(int position) {
+        ProductosAdapter.setSelected(position);
+        Toast.makeText(this, position + " id adapter: " + adaptadorProductos.getItem(position).getId() +
+                " id elemento: " + productos.get(position).getId(), Toast.LENGTH_LONG).show();
+        btAddToCarrito.setEnabled(true); //Ya existe un elemento seleccionado
+        adaptadorProductos.notifyDataSetChanged();
     }
 
-
+    private void addProductToCarrito() {
+        try {
+            if (dbManager.addProductToCarrito(
+                    productos.get(ProductosAdapter.selected).getId(),
+                    Integer.parseInt(etCant.getText().toString())
+            )) {
+                AddProductToCarritoActivity.this.setResult(RESULT_OK);
+                AddProductToCarritoActivity.this.finish();
+            } else {
+                Toast.makeText(this, R.string.no_stock, Toast.LENGTH_LONG).show();
+            }
+        } catch (NumberFormatException exception) {
+            Log.e("view.AddProductCarrito", exception.getMessage());
+        }
+    }
 
     private void onCancel(View v) {
         this.setResult(RESULT_CANCELED);
--- app/src/main/java/com/pmul/cajasupermercado/db/DBManager.java
+++ app/src/main/java/com/pmul/cajasupermercado/db/DBManager.java
@@ -75,6 +75,11 @@ public class DBManager extends SQLiteOpenHelper {
         currentDB = null;
     }
 
+    /**
+     * Sobreescribimos el método para que no haya conflicto de dos conexiones activas en caso de
+     * que todavía no se haya cerrado la del onCreate
+     * @return la instancia de la SQLiteDatabase
+     */
     @Override
     public SQLiteDatabase getWritableDatabase() {
         SQLiteDatabase db;
@@ -86,6 +91,11 @@ public class DBManager extends SQLiteOpenHelper {
         return db;
     }
 
+    /**
+     * Sobreescribimos el método para que no haya dos conexiones activas al llamarlo y así comprobar
+     * que la instancia del onCreate no entra en conflictos
+     * @return la instancia del SQLiteDatabase
+     */
     @Override
     public SQLiteDatabase getReadableDatabase() {
         SQLiteDatabase db;
@@ -97,6 +107,11 @@ public class DBManager extends SQLiteOpenHelper {
         return db;
     }
 
+    /**
+     * Método para crear unos cuantos productos básicos con los que empezar a hacer pruebas
+     * Se llama en el onCreate por lo que para reiniciar la base de datos con estos mismos productos
+     * tan solo habría que borrar los datos de la aplicación
+     */
     private void createBasics() {
         String [] names = {"Patata", "Tomate", "Pan", "Vino", "Leche", "Agua", "Huevos", "Harina", "Yogurt", "Chocolate"};
         Random stocks = new Random(15);
@@ -109,6 +124,11 @@ public class DBManager extends SQLiteOpenHelper {
         addProduct("Bajo de stock", 5, 10, 5.2);
     }
 
+    /**
+     * Método auxiliar para redondear double a dos decimales
+     * @param value el double que queremos redondear
+     * @return el double redondeado solo con dos decimales
+     */
     private double round(double value) {
       return  Math.round(value * 100.0) / 100.0;
     }
@@ -127,50 +147,6 @@ public class DBManager extends SQLiteOpenHelper {
             db.endTransaction();
         }
     }
-
-    /**
-     * Añade un producto a la base de datos, si ya existe actualiza el stock, está sobrecargado para reutilizar la db del onCrate
-     * @param db   base de datos
-     * @param name nombre del producto
-     * @param stock stock actual
-     * @param w_stock stock mínimo
-     * @param price precio
-     */
-    public void addProduct(SQLiteDatabase db, String name, int stock, int w_stock, double price) {
-        db = this.getWritableDatabase();
-        Cursor cursor = null;
-        ContentValues values = new ContentValues();
-
-        values.put(PRODUCTS_COL_NAME, name);
-        values.put(PRODUCTS_COL_PRICE, price);
-        values.put(PRODUCTS_COL_STOCK, stock);
-        values.put(PRODUCTS_COL_WARNING_STOCK, w_stock);
-
-        try {
-            db.beginTransaction();
-            cursor = db.query( TABLE_PRODUCTS,
-                    new String[] {PRODUCTS_COL_ID},
-                    PRODUCTS_COL_NAME + "= ?",
-                    new String[] { name }, null, null, null, "1");
-            if(cursor.getCount() > 0 ) {
-                db.update( TABLE_PRODUCTS,
-                        values,
-                        PRODUCTS_COL_ID + " = ?",
-                        new String [] {cursor.getString(0)});
-            } else {
-                db.insert(TABLE_PRODUCTS,
-                        null,
-                        values);
-            }
-            db.setTransactionSuccessful();
-        } catch (SQLException exc) {
-            Log.e("DBManager.addProducto", exc.getMessage());
-        } finally {
-            cursor.close();
-            db.endTransaction();
-        }
-    }
-
 
     /**
      * Añade un producto a la base de datos, si ya existe actualiza el stock
@@ -214,12 +190,89 @@ public class DBManager extends SQLiteOpenHelper {
         }
     }
 
+    /**
+     * Método que añade un producto al carrito, tan solo necesitamos que añada el id referencia y la
+     * cantidad de ese producto que queremos añadir al carrito, pero hay también que actualizar la
+     * tabla de productos restando la cantidad al stock total del mismo
+     * @param id del producto
+     * @param cantidad que queremos añadir al carrito
+     * @return verdadero en caso de que se haya podido llevar a cabo y falso en caso de si no hay
+     * suficiente stock o si ocurre algún otro problema que nos impide añadir el producto al carrito
+     */
+    public boolean addProductToCarrito(int id, int cantidad) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = null;
+        ContentValues values = new ContentValues();
+        values.put(CARRITO_COL_ID, id);
+        values.put(CARRITO_COL_CANT, cantidad);
+
+        try {
+            db.beginTransaction();
+            cursor = db.query(TABLE_PRODUCTS,
+                    new String[] {PRODUCTS_COL_STOCK},
+                    PRODUCTS_COL_ID + "= ?",
+                    new String[] {Integer.toString(id)}, null, null, null, "1");
+            Log.i("cursorlog", cursor.getInt(0) + " cursorlog " + cursor.getCount());
+            int currentStock = cursor.getInt(0);
+            if(currentStock < cantidad) {
+                Log.i(".addProductToCarrito", "No hay suficiente stock para añadir tal cantidad de ese producto");
+                return false;
+            } else {
+                db.insert(TABLE_CARRITO, null, values);
+                ContentValues updatedProduct = new ContentValues();
+                updatedProduct.put(PRODUCTS_COL_STOCK, currentStock - cantidad);
+                db.update(TABLE_PRODUCTS,
+                        updatedProduct,
+                        PRODUCTS_COL_ID + "= ?",
+                        new String[] {Integer.toString(id)});
+            }
+            db.setTransactionSuccessful();
+        } catch (SQLException exception) {
+            Log.e(".addProductToCarrito", exception.getMessage());
+            return false;
+        } finally {
+            cursor.close();
+            db.endTransaction();
+        }
+        return true;
+    }
+
+
+    /**
+     * Método que nos devuelve todos los productos disponibles en base de datos en un ArrayList
+     * @return el arraylist de productos
+     */
     public ArrayList<Producto> getAllProducts() {
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor cursor = db.query(DBManager.TABLE_PRODUCTS, null, null, null, null, null, PRODUCTS_COL_NAME);
         ArrayList<Producto> productos = new ArrayList<>();
         while(cursor.moveToNext()) {
             productos.add(new Producto(cursor.getInt(0), cursor.getString(1), cursor.getInt(2), cursor.getInt(4), cursor.getDouble(3)));
+        }
+        cursor.close();
+        return productos;
+    }
+
+    /**
+     * Método que nos devuelve todos los productos del carrito en un arraylist
+     * @return el arraylist de productos actuales en el carrito
+     */
+    public ArrayList<Producto> getAllCarrito() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(TABLE_CARRITO, null, null, null, null, null, null);
+        ArrayList<Producto> productos = new ArrayList<>();
+        while(cursor.moveToNext()) {
+            Cursor data = db.query(TABLE_PRODUCTS,
+                    new String[]{PRODUCTS_COL_NAME, PRODUCTS_COL_PRICE},
+                    CARRITO_COL_ID + "= ?",
+                    new String[]{Integer.toString(cursor.getInt(0))},
+                    null, null, null, "1");
+            productos.add( new Producto(cursor.getInt(0),
+                    data.getString(0),
+                    cursor.getInt(1),
+                    0,
+                    data.getDouble(1)));
+            data.close();
         }
         cursor.close();
         return productos;
